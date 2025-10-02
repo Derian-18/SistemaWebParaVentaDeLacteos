de631f7ade5292afe725d8c80565e5d017cfdd6c
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-10-01 22:53:06 -0700

    Primer commit

---
+++ codigofuente/core_main_productoslacteos/admin_core_productoslacteos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdminCoreProductoslacteosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_core_productoslacteos'
