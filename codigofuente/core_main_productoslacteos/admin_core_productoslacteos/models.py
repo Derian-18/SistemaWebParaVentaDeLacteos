a09151affbe2a6f0c75e3e181d57f949bcb739bc
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-10-10 09:51:22 -0700

    Laptop cambios

--- codigofuente/core_main_productoslacteos/admin_core_productoslacteos/models.py
+++ codigofuente/core_main_productoslacteos/admin_core_productoslacteos/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User, AbstractUser
 
 # Aqui es donde creare las tablas de mi base de datos.
 
-# No se ocupa crear esto importando la libreria User de Python
-# Tabla Usuario (Python ya lo crea solo agrego datos que necesito)
+# No se ocupa crear la tabla User importando la libreria User de Python
+# Tabla Usuario (Python ya lo crea, solo agrego datos que necesito)
 # Aqui extiendo con AbstractUser los datos que yo necesito a User de Django
 class Usuario_personalizado(AbstractUser):
     ROLES = (
