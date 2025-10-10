d187cb9f8f996f27b9c747e1860883dd38c61f22
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-10-10 00:41:36 -0700

    Informacion del equipo de desaarrollo

--- codigofuente/core_main_productoslacteos/admin_core_productoslacteos/urls.py
+++ codigofuente/core_main_productoslacteos/admin_core_productoslacteos/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import *
 
+# Si quiero que se muestre primero una pagina como por ejemplo la principal
+# Tengo que ponerla de primero en la urlpatterns
+
 urlpatterns = [
-    path('', home, name='home'),   
-]
+    # Aqui va a ir la pagina sobre la tienda web
+    path('', landingpage, name="landingpage"),
+    
+    # Esta es la pagina sobre nosotros los desarrolladores
+    path('nosotros/', Nosotros, name='Nosotros') 
+    
+]   
--- codigofuente/core_main_productoslacteos/admin_core_productoslacteos/views.py
+++ codigofuente/core_main_productoslacteos/admin_core_productoslacteos/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Aqui iran todas mis vistas
 
 # Esta es la landing pague del proyecto de productos lacteos
-def home(request):
+def landingpage(request):
     return render(request, 'landing_page.html')
 
 
-def landing_pague(request):
-    return render(request, 'base.html')
+def Nosotros(request):
+    return render(request, 'Nosotros.html')
