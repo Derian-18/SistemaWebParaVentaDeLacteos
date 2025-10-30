1ce1e6fc63642500f82849a05b966705dd4ac7e2
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-10-29 18:20:51 -0700

    Se agrego app para CRUD de productos

---
+++ codigofuente/core_main_productoslacteos/pedidos/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Importamos las demas tablas necesarias de las otras Apps 
+from admin_core_productoslacteos.models import Usuario_personalizado
+from productos.models import Producto
+
+# Create your models here.
+
+# Tabla Pedidos
+class Pedido(models.Model):
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+    ESTADO = (
+        ('pendiente', 'Pendiente'),
+        ('procesando', 'Procesando'),
+        ('completado', 'Completado'),
+        ('cancelado', 'Cancelado'),
+    )
+    estado = models.CharField(max_length=50, choices=ESTADO, default='pendiente')
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    metodo_pago = models.CharField(max_length=50)
+    fecha_pago = models.DateTimeField(null=True, blank=True)
+    # Esta es la relacion con la tabla Usuario
+    usuario = models.ForeignKey(Usuario_personalizado, on_delete=models.CASCADE)
+
+# Tabla pedidos_detalle
+class PedidoDetalle(models.Model):
+    cantidad = models.PositiveIntegerField()
+    precio_compra = models.DecimalField(max_digits=10, decimal_places=2)
+    # Esta es la relacion con la tabla Pedidos
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    # Esta es la relacion con la tabla Producto
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
