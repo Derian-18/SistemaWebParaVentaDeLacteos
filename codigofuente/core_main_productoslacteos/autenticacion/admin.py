e5a8a006d09d7c5339e71de8ccffcb2bbb42953c
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-11-15 00:03:10 -0800

    Se agregaron los campos que salen al crear un usuario en el panel admin

--- codigofuente/core_main_productoslacteos/autenticacion/admin.py
+++ codigofuente/core_main_productoslacteos/autenticacion/admin.py
@@ -2,4 +2,28 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usuario_personalizado
 
-admin.site.register(Usuario_personalizado, UserAdmin)
+class UsuarioPersonalizadoAdmin(UserAdmin):
+    model = Usuario_personalizado
+
+    # Campos que aparecerán en el admin
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Información Personal', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas Importantes', {'fields': ('last_login', 'date_joined')}),
+        ('Rol del usuario', {'fields': ('rol',)}),
+    )
+
+    # Campos que salen al crear un usuario nuevo
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'rol', 'is_staff', 'is_active')}
+        ),
+    )
+
+    list_display = ['username', 'email', 'rol', 'is_staff', 'is_active']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+
+admin.site.register(Usuario_personalizado, UsuarioPersonalizadoAdmin)
